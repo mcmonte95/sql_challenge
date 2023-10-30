199a09f3ee409b1a7d93c13f90c1a678760f4c1f
mcmonte95 <mike.chiaramonte95@gmail.com>
2023-10-29 23:14:02 -0400

    table schema created

---
+++ EmployeeSQL/schema_sql_challenge.sql
@@ -0,0 +1,52 @@
+-- Create departments table 
+CREATE TABLE departments (
+	dept_no VARCHAR(255) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(255)
+);
+
+-- create titles table
+CREATE TABLE titles (
+	title_id VARCHAR(255) PRIMARY KEY NOT NULL,
+	title VARCHAR(255)
+);
+
+-- create employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(255) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	birth_date DATE,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	sex VARCHAR(255),
+	hire_date DATE
+);
+
+-- create salaries table
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- create dept_manager table (create composite PK)
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(255) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+-- create dept_emp table (create composite PK)
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(255) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+
+
+
